d7252c2e3e64f863a31cc30f800b8b041deffcac
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-10-15 16:28:03 -0400

    solution 2 with regexp

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -11,11 +11,18 @@ function vowels(str) {
     let counter = 0
     let checker = 'aeiou'.split('') // turn string into an array
     for (let char of str.toLowerCase()){
+        // Using includes to determine if the current character could be found 
+        // inside of the checker array
         if (checker.includes(char)){
             counter++
         }
     }
     return counter
+}
+
+function vowels(str){
+    let matches = str.match(/[aeiou]/gi)
+    matches ? matches.length : 0
 }
 
 // vowels('Hi There')
